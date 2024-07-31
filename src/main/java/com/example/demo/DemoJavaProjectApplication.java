09bdf311194aebeb4d7f1aad60cea5afbbb6d354
ada88 <alidoganaydin88@gmail.com>
2024-07-31 15:59:27 +0300

    Update DemoJavaProjectApplication.java

--- src/main/java/com/example/demo/DemoJavaProjectApplication.java
+++ src/main/java/com/example/demo/DemoJavaProjectApplication.java
@@ -2,6 +2,9 @@ package com.example.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
 public class DemoJavaProjectApplication {
@@ -9,5 +12,10 @@ public class DemoJavaProjectApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(DemoJavaProjectApplication.class, args);
 	}
+
+	@GetMapping("/hello")
+    public String hello(@RequestParam(value = "name", defaultValue = "World") String name) {
+      return String.format("Hello %s!", name);
+    }
 
 }
