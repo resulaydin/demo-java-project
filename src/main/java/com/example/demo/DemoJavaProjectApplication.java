4896641bd5b522d1199ad5616e6658513c2bfb47
ada88 <alidoganaydin88@gmail.com>
2024-07-31 16:09:14 +0300

    Update DemoJavaProjectApplication.java

--- src/main/java/com/example/demo/DemoJavaProjectApplication.java
+++ src/main/java/com/example/demo/DemoJavaProjectApplication.java
@@ -7,6 +7,7 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@ComponentScan(basePackages = "com.example.demo")
 public class DemoJavaProjectApplication {
 
 	public static void main(String[] args) {
