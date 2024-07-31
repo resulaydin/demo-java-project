c462efe0fbc9480c3dc6de3106ced6efb91fa608
ada88 <alidoganaydin88@gmail.com>
2024-07-31 16:06:10 +0300

    Update DemoJavaProjectApplication.java

--- src/main/java/com/example/demo/DemoJavaProjectApplication.java
+++ src/main/java/com/example/demo/DemoJavaProjectApplication.java
@@ -13,7 +13,7 @@ public class DemoJavaProjectApplication {
 		SpringApplication.run(DemoJavaProjectApplication.class, args);
 	}
 
-	@GetMapping("/hello")
+	@GetMapping("/")
     public String hello(@RequestParam(value = "name", defaultValue = "World") String name) {
       return String.format("Hello %s!", name);
     }
